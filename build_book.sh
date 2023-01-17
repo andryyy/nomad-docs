1f03f073ecd01b59a04d9c64693568bdc4a1c068
andryyy <andre.peters@debinux.de>
2023-01-17 17:49:36 +0100

    Di 17. Jan 17:49:36 CET 2023

--- build_book.sh
+++
@@ -1,8 +0,0 @@
-./mdbook build book
-git add -A
-git commit -m "$(date)"
-git push origin main
-echo Waiting a few seconds before pulling data from GH via job file...
-sleep 10
-cd ../jobs/nginx
-bash run_nginx.sh -force
